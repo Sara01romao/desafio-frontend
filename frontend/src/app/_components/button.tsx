982313acaf0cf305319d4e1fcac14aefec937f4c
Sara Romao <sara-romao@live.com>
2025-08-10 18:58:28 -0300

    removed space lines

--- frontend/src/app/_components/button.tsx
+++ frontend/src/app/_components/button.tsx
@@ -1,10 +1,8 @@
-
 type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
   className?: string;
-
 }
 
 export function Button({ children, type, onClick, className, ...props }: ButtonProps) {
@@ -14,7 +12,6 @@ export function Button({ children, type, onClick, className, ...props }: ButtonP
       onClick={onClick}
       className={className}
       {...props}
-      
     >
       {children}
     </button>
