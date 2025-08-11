a1dfc70b650a670fb042542d90582e2ee69dfe95
Sara Romao <sara-romao@live.com>
2025-08-11 17:57:46 -0300

    modified z-index, width cripto value and padding btn add

--- frontend/src/app/_components/modal.tsx
+++ frontend/src/app/_components/modal.tsx
@@ -8,7 +8,7 @@ type ModalProps = {
 
 export function Modal({ setActiveModal }: ModalProps) {
   return (
-    <div className="bg-gray-950/70 fixed top-0 left-0 h-full max-w-[100%] w-full flex justify-center items-center p-2">
+    <div className="bg-gray-950/70 fixed top-0 left-0 h-full max-w-[100%] z-50 w-full flex justify-center items-center p-2">
       <div className="bg-white max-w-[600px] w-full rounded-lg p-12">
         <h2 className="font-bold border-b-1 border-[#F1F1F2] text-[#3A3A3A] text-xl mb-10 pb-4">Adicionar Carteira</h2>
 
@@ -16,9 +16,10 @@ export function Modal({ setActiveModal }: ModalProps) {
           <Input type="text" name="name" label="Nome" placeholder="Nome" />
           <Input type="text" name="sobrenome" label="Sobrenome" placeholder="Sobrenome" />
           <Input type="email" name="email" label="Email" placeholder="Email" />
+          
           <div className="flex gap-6 items-center">
-            <Input type="text" name="valueBuy" label="Valor de compra" placeholder="Valor de compra" />
-            <h2 className="font-bold text-lg text-[#3A3A3A]">BTC 0.12345</h2>
+            <Input type="number" name="valueBuy" label="Valor de compra" placeholder="Valor de compra" />
+            <h2 className="font-bold text-xl text-[#3A3A3A] max-w-[300px] w-full">BTC 0.12345</h2>
           </div>
 
           <div className="flex justify-end gap-4 mt-4">
@@ -26,7 +27,7 @@ export function Modal({ setActiveModal }: ModalProps) {
               Cancelar
             </Button>
 
-            <Button type="button" className="bg-[#007BFF] text-white py-2 px-4 rounded-sm cursor-pointer hover:bg-[#0C9BF2]" >
+            <Button type="button" className="bg-[#007BFF] text-white py-2 px-6 rounded-sm cursor-pointer hover:bg-[#0C9BF2]" >
               Adicionar
             </Button>
           </div>
