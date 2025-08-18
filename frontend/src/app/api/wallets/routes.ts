32fa4dea411fd6b73223b113f34460257afacc2c
Sara Romao <sara-romao@live.com>
2025-08-18 12:45:17 -0300

    config post request for new wallet

--- frontend/src/app/api/wallets/routes.ts
+++ frontend/src/app/api/wallets/routes.ts
@@ -1,6 +1,32 @@
 'use server'
 
 import { walletTypes } from "@/app/page";
+import { randomUUID } from "crypto";
+
+export interface NewWalletTypes {
+  nome: string;
+  sobrenome: string;
+  email: string;
+  valor_carteira: number;
+}
+
+export async function postWallet(wallet: NewWalletTypes) {
+  const id = randomUUID();
+  const infoUse = {
+    "id": id,
+    "endereco": "72100 Mockingbird Lane",
+    "data_nascimento": "1997-09-06T14:03:52Z",
+    "data_abertura": "2017-02-18T01:10:29Z",
+    "endereco_carteira": "1MqurpDATqHNDUPMVbR7L3BW1hz2DcM29"
+  }
+  
+  console.log("post", wallet)
+  const response = await fetch(`http://localhost:3004/users`, {
+    method: "POST",
+    body: JSON.stringify(Object.assign(infoUse, wallet))
+  });
+  return response.ok;
+}
 
 export async function updatedWallet(wallet: walletTypes) {
   const response = await fetch(`http://localhost:3004/users/${wallet.id}`, {
