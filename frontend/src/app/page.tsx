4a077731ada750b6dde027979da9c5222c217c09
Sara Romao <sara-romao@live.com>
2025-08-19 10:58:35 -0300

    modified tr table and add search result and filter

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,6 @@
 import { useEffect, useState } from "react";
 import { Button } from "./_components/button";
 import { ModalAdd } from "./_components/modalAdd";
-import { Input } from "./_components/input";
-import { IoMdSearch } from "react-icons/io";
 import { MdOutlineModeEdit } from "react-icons/md";
 import { FiTrash } from "react-icons/fi";
 import { IoIosArrowBack } from "react-icons/io";
@@ -12,6 +10,7 @@ import { IoIosArrowForward } from "react-icons/io";
 import { Tooltip } from "./_components/tooltip";
 import { EditModal } from "./_components/modalEdit";
 import { DeleteModal } from "./_components/modalDelete";
+import Filter from "./_components/filter";
 
 export interface walletTypes {
   id: string;
@@ -21,7 +20,7 @@ export interface walletTypes {
   valor_carteira: number;
 }
 
-interface dataType {
+export interface dataType {
   data: walletTypes[];
   first: number;
   items: number;
@@ -39,7 +38,8 @@ export default function Home() {
   const [page, setPage] = useState<number>(1);
   const [totalPages, setTotalPage] = useState<number>(1);
   const [wallet, setWallet] = useState<walletTypes>();
-  const [isResponse, setIsResponse] = useState(false)
+  const [isResponse, setIsResponse] = useState(false);
+  const [isSearch, setIsSearch] = useState<walletTypes[]>([]);
 
   useEffect(() => {
     async function dataWallet() {
@@ -52,8 +52,8 @@ export default function Home() {
       setTotalPage(data.pages);
       setIsResponse(false);
     }
-    dataWallet()
-  }, [page,isResponse]);
+    dataWallet();
+  }, [page, isResponse]);
 
   function handleButtonPage() {
     let buttonsPage = [];
@@ -94,20 +94,7 @@ export default function Home() {
         </Button>
       </div>
 
-      <form action="" className="flex flex-col lg:flex-row gap-4 bg-white p-4 mt-10 rounded-md shadow-xl">
-        <div className="flex w-full flex-col  lg:flex-row gap-4">
-          <Input type="text" name="name" label="Nome" placeholder="Nome" />
-          <Input type="text" name="sobrenome" label="Sobrenome" placeholder="Sobrenome" />
-          <Input type="email" name="email" label="Email" placeholder="Email" />
-        </div>
-        <Button
-          onClick={() => alert("teste")}
-          type="submit"
-          className="flex items-center justify-center lg:justify-start gap-2 border-2 border-solid border-[#007BFF] px-4 py-2 font-w text-[#007BFF] rounded-sm font-medium cursor-pointer hover:text-[#0C9BF2] hover:border-[#0C9BF2]">
-          <IoMdSearch />
-          Buscar
-        </Button>
-      </form>
+      <Filter setSearch={setIsSearch} setResponse={setIsResponse} />
 
       <div className="bg-white rounded-md mt-10 py-4 py-10">
         <div className="flex items-center px-8">
@@ -131,16 +118,15 @@ export default function Home() {
                 <th className="text-[#3A3A3A] text-start pb -2"></th>
               </tr>
             </thead>
-            <tbody className="">
-              {dataWallet && (
-                dataWallet.data.map(wallet => (
+            <tbody>
+              {isSearch && (isSearch.length > 0 ?
+                isSearch.map(wallet => (
                   <tr key={wallet.id} className="odd:bg-white even:bg-[#F5F6F8] hover:bg-[#FAFDFF]  ">
                     <td className="p-2">{wallet.nome}</td>
                     <td className="p-2">{wallet.sobrenome}</td>
                     <td className="p-2">{wallet.email}</td>
                     <td className="p-2">{wallet.valor_carteira.toFixed(8)}</td>
                     <td className="flex gap-2 p-2 justify-end">
-
                       <div className="relative flex flex-col group">
                         <button onClick={() => handleEditWallet(wallet)} className="p-1 relative cursor-pointer group hover:bg-[#F5F6F8] transition duration-700 ease-in-out rounded-full p-2">
                           <MdOutlineModeEdit size={18} className='text-[#767676]' />
@@ -157,6 +143,33 @@ export default function Home() {
                     </td>
                   </tr>
                 ))
+                : 
+                dataWallet && (
+                  dataWallet.data.map(wallet => (
+                    <tr key={wallet.id} className="odd:bg-white even:bg-[#F5F6F8] hover:bg-[#FAFDFF]  ">
+                      <td className="p-2">{wallet.nome}</td>
+                      <td className="p-2">{wallet.sobrenome}</td>
+                      <td className="p-2">{wallet.email}</td>
+                      <td className="p-2">{wallet.valor_carteira.toFixed(8)}</td>
+                      <td className="flex gap-2 p-2 justify-end">
+
+                        <div className="relative flex flex-col group">
+                          <button onClick={() => handleEditWallet(wallet)} className="p-1 relative cursor-pointer group hover:bg-[#F5F6F8] transition duration-700 ease-in-out rounded-full p-2">
+                            <MdOutlineModeEdit size={18} className='text-[#767676]' />
+                          </button>
+                          <Tooltip>Editar</Tooltip>
+                        </div>
+
+                        <div className="relative flex flex-col group">
+                          <button onClick={() => (handleDeleteWallet(wallet))} className="p-1 cursor-pointer group hover:bg-[#F5F6F8] transition duration-700 ease-in-out rounded-full p-2">
+                            <FiTrash size={18} className='text-[#767676]' />
+                            <Tooltip>Excluir</Tooltip>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))
+                )
               )}
             </tbody>
           </table>
